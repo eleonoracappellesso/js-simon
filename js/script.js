89e7fcaba7e90331349577e6502837a7717d53d8
eleonoracappellesso <cappellessoeleonora@gmail.com>
2024-11-11 16:46:21 +0100

    added event listener to confirm button to take inputs numbers

--- js/script.js
+++ js/script.js
@@ -1,23 +1,28 @@
 "use strict";
 console.clear();
 
-let i = 0;
+let counter = 0;
 const numbersList = document.getElementById('numbers-list');
 const answersForm = document.getElementById('answers-form');
 
 let seeCountdown = document.getElementById('countdown');
 let seconds = 10;
+
+const userNumbers = [];
+const confirmBtn = document.getElementById('confirmBtn');
+const inputs = document.querySelectorAll('input');
+
 // faccio apparire un countdown
 const timer = setInterval(function () {
     //finchè i secondi sono maggiori di 0 genero 5 num random con un ciclo while e li stampo all'interno di un li creato nella ul
     if (seconds > 0) {
         seeCountdown.innerHTML = --seconds;
-        while (i < 5) {
-            const randomNumber = getRndInteger(1, 100);
+        while (counter < 5) {
+            const randomNumber = getRndInteger(1, 50);
             let newLi = document.createElement('li');
             newLi.innerText = randomNumber;
             numbersList.append(newLi);
-            i++;
+            counter++;
             // console.log(newLi);
         }
     } //quando i secondi arrivano a 0 allora rimuovo il timer e nascondo i numeri random e faccio apparire il form per l'utente
@@ -25,9 +30,20 @@ const timer = setInterval(function () {
         clearInterval(timer);
         numbersList.classList.add('d-none');
         seeCountdown.classList.add('d-none');
-
         answersForm.classList.remove('d-none');
+
+        // prendo i valori degli input del form e li metto in un array al click
+        confirmBtn.addEventListener('click', function (event) {
+            for (let i = 0; i < inputs.length; i++) {
+                userNumbers.push(parseInt(inputs[i].value));
+                console.log(userNumbers);
+            }
+            event.preventDefault;
+        });
     }
 }, 1000);
+
+
+
 
 
